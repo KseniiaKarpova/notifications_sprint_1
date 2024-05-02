d8f20b025a7273e55e2e13462cb75ca71bb858d8
NozirTukhtamurodov <nozir9407@gmail.com>
2024-05-02 16:19:42 +0500

    bug fix in get users and create super user

--- AuthAPI/cli_create_super_user.py
+++ AuthAPI/cli_create_super_user.py
@@ -15,36 +15,33 @@ settings = config.APPSettings()
 logging.getLogger('asyncio').setLevel(logging.WARNING)
 
 
-def create(login: str, password: str):
-    try:
-        async def save():
-            postgres.async_engine = create_async_engine(
-                settings.db_dsn,
-                poolclass=QueuePool,
-                pool_pre_ping=True, pool_size=20, pool_timeout=30)
+def create(login: str, password: str, email: str):
+    async def save():
+        postgres.async_engine = create_async_engine(
+            settings.db_dsn,
+            poolclass=QueuePool,
+            pool_pre_ping=True, pool_size=20, pool_timeout=30)
 
-            postgres.async_session_factory = sessionmaker(
-                postgres.async_engine,
-                expire_on_commit=False,
-                autoflush=True,
-                class_=AsyncSession)
+        postgres.async_session_factory = sessionmaker(
+            postgres.async_engine,
+            expire_on_commit=False,
+            autoflush=True,
+            class_=AsyncSession)
 
-            hashed_password = await DataHasher().generate_word_hash(secret_word=password)
-            storage = UserStorage(session=postgres.create_async_session())
-            await storage.create(params={
+        hashed_password = await DataHasher().generate_word_hash(secret_word=password)
+        async with postgres.async_session_factory() as session:
+            storage = UserStorage(commit_mode=False)
+            instance = await storage.create(params={
                 'password': hashed_password,
                 'login': login,
                 'is_superuser': True,
+                'email': email,  
             })
-            await postgres.async_engine.dispose()
-
-        asyncio.run(save())
-
-        print(f"Creating Super User: {login}")
-
-    except Exception as e:
-        print('Can`t create Super User')
-        print(e)
+            session.add(instance)
+            await session.commit()
+        await postgres.async_engine.dispose()
+    asyncio.run(save())
+    print(f"Creating Super User: {login}")
 
 
 if __name__ == "__main__":
