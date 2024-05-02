05e7d8d8b8f2442842a2b051077109b40d568507
NozirTukhtamurodov <nozir9407@gmail.com>
2024-05-02 18:16:45 +0500

    superuser create added

--- AuthAPI/cli_create_super_user.py
+++ AuthAPI/cli_create_super_user.py
@@ -5,7 +5,10 @@ import typer
 from core import config
 from core.hasher import DataHasher
 from db import postgres
+from sqlalchemy import QueuePool
+from sqlalchemy.orm import sessionmaker
 from storages.user import UserStorage
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 
 
 settings = config.APPSettings()
@@ -14,6 +17,17 @@ logging.getLogger('asyncio').setLevel(logging.WARNING)
 
 def create(login: str, password: str, email: str):
     async def save():
+        postgres.async_engine = create_async_engine(
+            settings.db_dsn,
+            poolclass=QueuePool,
+            pool_pre_ping=True, pool_size=20, pool_timeout=30)
+
+        postgres.async_session_factory = sessionmaker(
+            postgres.async_engine,
+            expire_on_commit=False,
+            autoflush=True,
+            class_=AsyncSession)
+
         hashed_password = await DataHasher().generate_word_hash(secret_word=password)
         async with postgres.async_session_factory() as session:
             storage = UserStorage(commit_mode=False)
@@ -25,8 +39,10 @@ def create(login: str, password: str, email: str):
             })
             session.add(instance)
             await session.commit()
+        await postgres.async_engine.dispose()
     asyncio.run(save())
     print(f"Creating Super User: {login}")
+
 
 if __name__ == "__main__":
     typer.run(create)
