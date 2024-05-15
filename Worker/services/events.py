1c3cc0a6294f5671d16d41a8b6bf1e6d177fe7be
NozirTukhtamurodov <nozir9407@gmail.com>
2024-05-15 19:06:29 +0500

    mass notification logic added

--- Worker/services/events.py
+++ Worker/services/events.py
@@ -1,6 +1,7 @@
 from storages.template import TemplateStorage
 from storages.history import HistoryStorage
 from models.messages import LogMessage
+from models.template import TemplateModel
 from services import BaseService
 from services.auth import AuthService, super_user_login_required
 from uuid import UUID
@@ -14,7 +15,7 @@ from models import TypeMessage, EventMessage
 
 class WebsocketService(BaseService):
     @super_user_login_required
-    async def notify_user(self, user_id: UUID, text: str, access_token: str):
+    async def notify(self, user_id: UUID, text: str, access_token: str):
         session = ClientSession(headers = {'Authorization': f'Bearer {access_token}'})
         async with session.post(
             url=settings.websocket.send_message_url,
@@ -40,22 +41,40 @@ class EventHandlerService(BaseService):
         self.logger = logger
         self.auth_service = auth_service
 
-    async def proceed(self, sender_id: UUID, reciver_id: UUID,event: str):
+    async def proceed(self, sender_id: UUID, reciver_id: UUID, event: str):
         template_obj = await self.storage.get(event=event, type=TypeMessage.notify.value)
         user_data = await self.auth_service.get_user_by_id(user_id=sender_id)
+        text_to_send = await self.compile_text(user=user_data, template=template_obj.template)
+        await self.ws_notifier.notify(user_id=reciver_id, text=text_to_send)
 
-        user_name = user_data.get('name')
-        user_surname = user_data.get('surname')
+    async def mass_notification(self, template: str):
+        get = True
+        page = 1
+        while get is True:
+            response: dict = await self.auth_service.get_users(page=page, size=100)
+            page += 1
+            users = response.get('items')
+            if not users:
+                get = False
+            await self.notify_users(users=users, template=template)
+
+    async def notify_users(self, users: list, template: str):
+        for user in users:
+            text_to_send = await self.compile_text(user=user, template=template)
+            await self.ws_notifier.notify(user_id=user['uuid'], text=text_to_send)
+            await self.logger.add(data=LogMessage(
+                user=user['uuid'],
+                type=TypeMessage.notify.value,
+                text=text_to_send,
+            ))
+            print(f"{text_to_send} was sent to {user}")
+
+    async def compile_text(self, user: dict, template: str):
+        user_name = user.get('name')
+        user_surname = user.get('surname')
         name = ''.join(user_name) if user_name else ''
         name.join(user_surname) if user_surname else ''
-        text_to_send = template_obj.template.format(user=name)
-        print(text_to_send)
-        await self.logger.add(data=LogMessage(
-            user=reciver_id,
-            type=TypeMessage.notify.value,
-            text=text_to_send,
-        ))
-        await self.ws_notifier.notify_user(user_id=reciver_id, text=text_to_send)
+        return template.format(user=name)
 
 
 def get_event_service():
--- Worker/services/periodic_notify.py
+++ Worker/services/periodic_notify.py
@@ -6,22 +6,22 @@ from fastapi_utilities import repeat_every
 from api.v1.notifications import router
 from faststream.rabbit import RabbitBroker
 from core.config import settings
+from schemas import InfoSchema
+
+
 broker = RabbitBroker(settings.rabbit.dsn)
 
 
-
-@repeat_every(seconds=3)
+@repeat_every(seconds=20)
 async def send_periodic_notify():
-    print('#######started########')
     storage = TemplateStorage()
     data = await storage.get_all()
     now = datetime.now().strftime("%d/%m/%Y %H:%M")
-    await router.broker.publish(queue='info', message='asdsdsds')
     for item in data:
 
         if check_now_is_valid_cron(item.schedule):
             #отправить на рассылку сообщение
-            pass
+            await router.broker.publish(queue='info', message=InfoSchema(template=item.template))
 
         else:
             d = item.date_send.strftime("%d/%m/%Y %H:%M")
